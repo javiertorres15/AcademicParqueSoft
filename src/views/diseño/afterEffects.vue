eef1bbdf67c34a98c24192e721d795c82eea9825
Your Name <javier.torres@cecar.edu.co>
2023-09-14 16:17:05 -0500

    Nuevo nuevo

--- src/views/diseño/afterEffects.vue
+++ src/views/diseño/afterEffects.vue
@@ -1,15 +1,226 @@
 <template>
-    <div>
-      <InicioDis />
+<div>
+    <InicioDis />
+</div>
+<div class="content-section-b">
+
+    <div class="container">
+
+        <div class="row">
+            <div class="col-lg-5 col-lg-offset-1 col-sm-push-6  col-sm-6">
+                <hr class="section-heading-spacer">
+                <div class="clearfix"></div>
+                <h2 class="section-heading">Presentación del curso:<br></h2>
+                <p class="lead">En este curso de Java aprenderás a dominar este lenguaje de
+                    programación básico, desde los conceptos, y poniendo en práctica ejemplos funcionales
+                    para su aprendizaje.</p>
+                <p class="lead">
+                    Aprenderás las sentencias condicionales y repetitivas, que te van a permitir desarrollar tu
+                    lógica de programación con éxito y la teoría base para poner en práctica con ejemplos
+                    sobre la Programación Orienta a Objetos con JAVA.
+                </p>
+            </div>
+            <div class="col-lg-5 col-sm-pull-6  col-sm-6">
+                <div class="row">
+                    <div class="col-sm-12 col-xl-4 ">
+                        <div class="card">
+                            <div class="face front">
+                                <img src="@/assets/imagenes/programacion/metodologia.png" alt="">
+                                <h3>Metodología</h3>
+                            </div>
+
+                            <div class="face back">
+                                <h3>Metodología</h3>
+                                <p>Educación a distancia en modalidad híbrida</p>
+                                <div class="link">
+
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
     </div>
-  </template>
+    <!-- /.container -->
+
+</div>
+
+<a name="services"></a>
+<div class="content-section-a">
+
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-5 col-lg-offset-1 col-sm-push-6  col-sm-6">
+                <div class="row">
+                    <div class="col-sm-12 col-xl-4 ">
+                        <div class="card">
+                            <div class="face front">
+                                <img src="@/assets/imagenes/programacion/dirigido.png" alt="">
+                                <h3>Duración</h3>
+                            </div>
+
+                            <div class="face back">
+                                <h3>Duración</h3>
+                                <p></p>
+                                <div class="link">
+
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-5 col-sm-pull-6  col-sm-6">
+                <div class="row">
+                    <hr class="section-heading-spacer">
+                    <div class="clearfix"></div>
+                    <h2 class="section-heading">Dirigido a:<br></h2>
+                    <p class="lead">Este curso es perfecto para principiantes sin experiencia en programación,
+                        orientado a cualquier persona con deseo de aprender a programar en JAVA.</p>
+                    <p class="lead">Estudiantes de carreras de informática, sistemas y programación, que quieran
+                        aprender a desarrollar una lógica de programación creando pequeñas aplicaciones
+                        en JAVA.</p>
+                    <p class="lead">Persona con ganas de iniciarse de verdad en la programación, aprendiendo el
+                        paradigma orientado a objetos con JAVA, y quieran algún día hacer brillar su genio
+                        interior para crear cosas formidables.</p>
+                </div>
+            </div>
+        </div>
+
+    </div>
+    <!-- /.container -->
+
+</div>
+
+<!-- /.content-section-a -->
+
+<div class="container">
+    <h1>Valor</h1>
+    <h3>$55.000</h3>
+    <div class="" ref="dropdownContainer1"> </div>
+
+</div>
+
+<div id="service" class="service">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-7">
+                <div class="titlepage">
+                    <h2><strong class="yellow">Contenidos</strong><br>Curso de programación basica en Java</h2>
+                </div>
+            </div>
+        </div>
+
+        <br><br>
+        <div class="row">
+            <div class="col-md-4 col-sm-6">
+                <div id="ho_color" class="service_box">
+                    <img src="@/assets/imagenes/programacion/servicios/icon-java.png" />
+                    <h3>Unidad 1: Lenguaje Java </h3>
+                    <p>1.1. Introducción al curso.</p>
+                    <p>1.2. ¿Qué es Java?</p>
+                    <p>1.3. Introducción e instalación
+                        del entorno.</p>
+                    <p>1.4. ¿Qué es un programa en
+                        Java?</p>
+                </div>
+            </div>
+            <div class="col-md-4 col-sm-6">
+                <div id="ho_color" class="service_box">
+                    <img src="@/assets/imagenes/programacion/servicios/icon-java.png" />
+                    <h3>Unidad 2: Variables y operadores</h3>
+                    <p>2.1. Variable y tipos de datos. </p>
+                    <p>2.2. Operadores.</p>
+                    <p>2.3. Operadores relacionales. </p>
+                    <p>2.4. Conversión de tipos </p>
+                </div>
+            </div>
+            <div class="col-md-4 col-sm-6">
+                <div id="ho_color" class="service_box">
+                    <img src="@/assets/imagenes/programacion/servicios/icon-java.png" />
+                    <h3>Unidad 3: Sentencias condicionales y
+                        repetitivas.</h3>
+                    <p>3.1. Sentencia condicional if.</p>
+                    <p>3.2. Sentencia selectiva switch.</p>
+                    <p>3.3. Sentencia repetitiva for.</p>
+                    <p>3.4. Sentencias repetitivas while y do while </p>
+                    <p>3.5. Ejercicio de Sentencias y Bucles </p>
+                </div>
+            </div>
+            <div class="col-md-4 col-sm-6">
+                <div id="ho_color" class="service_box">
+                    <img src="@/assets/imagenes/programacion/servicios/icon-java.png" />
+                    <h3>Unidad 4: Excepciones, funciones y
+                        recursividad.</h3>
+                    <p>4.1. Excepciones.</p>
+                    <p>4.2. Funciones.</p>
+                    <p>4.3. Sobrecarga de funciones</p>
+                    <p>4.4. Recursividad</p>
+                </div>
+            </div>
+            <div class="col-md-4 col-sm-6">
+                <div id="ho_color" class="service_box">
+                    <img src="@/assets/imagenes/programacion/servicios/icon-java.png" />
+                    <h3>Unidad 5: Programación orientada a
+                        Objetos.</h3>
+                    <p>5.1. Clases y Objetos. </p>
+                    <p>5.2. Herencia. </p>
+                    <p>5.3. Constructores. </p>
+                    <p>5.4. Uso de This y Super. </p>
+                    <p>5.5. Polimorfismo. </p>
+                    <p>5.6. Clases Abstractas e Interfaces. </p>
+                    <p>5.7. Ejercicio de Clases y Objetos.</p>
+
+                </div>
+            </div>
+            <div class="col-md-4 col-sm-6">
+                <div id="ho_color" class="service_box">
+                    <img src="@/assets/imagenes/programacion/servicios/icon-java.png" alt="#" />
+                    <h3>Unidad 6: Arrays-Arreglos.</h3>
+                    <p>6.1. Arreglos Unidimensionales. </p>
+                    <p>6.2. Arreglos Bidimensionales. </p>
+                    <p>6.3. Ejercicio de Arreglos y Clases. </p>
+                    <p>6.4. Analizamos el ejercicio.</p>
+                    <p>6.5. Creamos la clase Atleta. </p>
+                    <p>6.6. Creamos la clase competencia. </p>
+                </div>
+            </div>
+
+        </div>
+    </div>
+</div>
+</template>
+
   
-  <script>
-  import InicioDis from './InicioDis.vue';
-  
-  export default {
+<script>
+import InicioDis from './InicioDis.vue';
+
+export default {
     components: {
         InicioDis,
     },
-  };
-  </script>
+    mounted() {
+        this.loadMercadoPagoScript(this.$refs.dropdownContainer1);
+        this.loadMercadoPagoScript(this.$refs.dropdownContainer2);
+        // Puedes cargar el script en más instancias si es necesario
+    },
+    methods: {
+        loadMercadoPagoScript(container) {
+            const script = document.createElement("script");
+            script.src = "https://www.mercadopago.com.co/integrations/v1/web-payment-checkout.js";
+            script.setAttribute("data-preference-id", "165907967-9f6a4af2-5ae1-4d9b-8cca-ab559e79bdb5");
+            script.setAttribute("data-source", "button");
+            container.appendChild(script);
+        }
+    }
+};
+</script>
+
+
+<style scoped>
+@import '../css/estiloCursos.css';
+
+@import '../css/bootstrap.min.css';
+</style>
--- src/views/pqrs.vue
+++ src/views/pqrs.vue
@@ -1,4 +1,4 @@
 <template>
 
-    
+<iframe src="https://docs.google.com/forms/d/e/1FAIpQLSdW6omzbWVmBa21xWJOuwDYXbkypEdldpECorqDiiF36n-DJA/viewform?embedded=true" style="width: 100%; height: 100vh; border: none;"></iframe>
 </template>
