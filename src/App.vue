ee4361034a41f119be23e09dbde50f29a3edc606
Your Name <javier.torres@cecar.edu.co>
2023-09-22 17:20:48 -0500

    hola

--- src/App.vue
+++ src/App.vue
@@ -9,7 +9,7 @@
 <header class="arriba">
     <nav class="navbar bg-body-tertiary">
         <div class="container-fluid">
-            <a class="navbar-brand scroll-logo" href="/"><img src="@/assets/imagenes/academic logo2p.png" alt="image"></a>
+            <a class="navbar-brand scroll-logo" href="/"><img src="@/assets/imagenes/academiclogo2p.png" alt="image"></a>
 
             <button class="navbar-toggler bg-white" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar" aria-label="Toggle navigation">
                 <span><i><img class="casa" src="@/assets/imagenes/Casa.png" alt=""></i></span>
